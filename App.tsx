cfae741dc6ba45b425b4fdc48d80031049963d57
Lucas <khoanad@gmail.com>
2024-01-04 17:20:31 +0700

    done lesson 1

--- App.tsx
+++ App.tsx
@@ -1,118 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.container}>
+      <View style={styles.containerTop}>
+          <View style={styles.top}>
+            <Text style={styles.topText}>Top Left</Text>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      <View style={styles.containerCenter}>
+        <View style={styles.center}>
+          <Text style={styles.centerText}>Center</Text>
+        </View>
+
+      </View>
+      <View style={styles.containerBottom}>
+        <View style={styles.bottom}>
+        <Text style={styles.bottomText}>Bottom Right</Text>
+        </View>
+      </View>
+
+    </View>
+  )
+
 }
+
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container:{
+    backgroundColor: '#e7feff',
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  containerTop:{
+    flex: 1
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  containerCenter:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  highlight: {
-    fontWeight: '700',
+  containerBottom:{
+    flex: 1
   },
-});
+  top:{
+    marginTop: 80,
+    marginHorizontal: 40,
+    backgroundColor: 'pink',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderWidth: 2,
+    borderColor: 'red'
+  },
+  topText: {
+    fontSize: 20,
+    color: 'blue',
+    fontWeight: 'bold'
+  },
+  center:{
+    width: 150,
+    height: 150,
+    backgroundColor: '#33ff60',
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  centerText:{
 
-export default App;
+  },
+  bottom:{
+    marginTop: 80,
+    marginHorizontal: 40,
+    backgroundColor: '#7ee6fd',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderWidth: 2,
+    borderColor: 'red',
+    borderRadius: 20
+  },
+  bottomText:{
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'right'
+  }
+
+})
